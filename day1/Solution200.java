763f1ff8f0c07ed14cae2c926bb3b79355deeabb
yong geng <yong.geng@vudu.com>
2019-11-07 17:16:19 -0800

    refactor code

--- day1/Solution200.java
+++ day1/Solution200.java
@@ -4,31 +4,31 @@ package day1;
  * Solution200
  */
 public class Solution200 {
-    
-    private int n;
-    private int m;
-
+        
     public int numIslands(char[][] grid) {
+        int n = 0;
+        int m = 0;
         int count = 0;
+        if (grid.length == 0 || grid[0].length == 0) return 0;
         n = grid.length;
-        if (n == 0) return 0;
         m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
+        for (int i=0; i<n; i++) {
+            for (int j=0;j<m; j++) {
+                if (grid[i][j] =='1') {
+                    DFSMarking(grid, i, j, n, m);
                     ++count;
-                }
-        }    
+                }                
+            }
+        }
         return count;
     }
     
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+    private void DFSMarking(char[][] grid, int i, int j, int n, int m) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+        DFSMarking(grid, i+1, j, n, m);
+        DFSMarking(grid, i-1, j, n, m);
+        DFSMarking(grid, i, j+1, n, m);
+        DFSMarking(grid, i, j-1, n, m);
     }
 }   
