ea3529cac2e2fb9f9052b2485381358177d810aa
yong geng <yong.geng@vudu.com>
2019-11-26 15:55:40 -0800

    add 105

---
+++ day6/Solution105.java
@@ -0,0 +1,17 @@
+package day6;
+
+import common.TreeNode;
+
+/**
+ * Solution105
+ */
+public class Solution105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, inorder);
+    }    
+
+    private TreeNode helper(int[] preorder, int[] inorder) {
+        
+        return null;
+    }
+}
