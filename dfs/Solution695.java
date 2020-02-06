0ccb959431e13dd765dc6b68163a494653119f0d
yong geng <yong.geng@vudu.com>
2020-02-05 16:49:04 -0800

    add 695

--- dfs/Solution695.java
+++ dfs/Solution695.java
@@ -8,30 +8,41 @@ public class Solution695 {
     public int maxAreaOfIsland(int[][] grid) {
         if (grid.length ==0 || grid[0].length == 0) {return 0;}
         int n = grid.length;
-        int m = grid.length;
-        int area = 0;
+        int m = grid[0].length;
         for (int i = 0 ; i < n ; i ++) {
             for (int j=0; j < m ; j ++) {
-                if (grid[i][j] == 1) {
-                    area ++ ;
-                    maxArea = Math.max(area,maxArea);
-                    dfs(grid, i, j, area);
+                if (grid[i][j] == 1) {                   
+                    maxArea = Math.max(dfs(grid, i, j),maxArea);                    
                 }
             }
         }
         return maxArea;
     }
     
-    void dfs(int[][] grid, int i, int j, int area) {
+    int dfs(int[][] grid, int i, int j) {
         int n = grid.length;
-        int m = grid.length;
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0) return;
-        area ++;
-        grid[i][j] = 0;
-        dfs(grid, i-1, j , area);
-        dfs(grid, i+1, j , area);
-        dfs(grid, i, j-1 , area);
-        dfs(grid, i, j+1, area);
+        int m = grid[0].length;
+        if (i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+        }
+        return 0;        
     }
     
+
+    // public int maxAreaOfIsland(int[][] grid) {
+    //     int max_area = 0;
+    //     for(int i = 0; i < grid.length; i++)
+    //         for(int j = 0; j < grid[0].length; j++)
+    //             if(grid[i][j] == 1)max_area = Math.max(max_area, AreaOfIsland(grid, i, j));
+    //     return max_area;
+    // }
+    
+    // public int AreaOfIsland(int[][] grid, int i, int j){
+    //     if( i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1){
+    //         grid[i][j] = 0;
+    //         return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+    //     }
+    //     return 0;
+    // }
 }
