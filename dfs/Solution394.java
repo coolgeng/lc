19b2b3c885d554a3d148bfda2aaba16e7e94c7b2
Cooper Geng <cooper.geng@fuhu.com>
2020-02-20 00:23:45 -0800

    re-org

--- dfs/Solution394.java
+++ dfs/Solution394.java
@@ -1,61 +1,13 @@
-package dfs;
-
-import java.util.Stack;
+package day6;
 
 /**
  * Solution394
  */
 public class Solution394 {
-    public String decodeString(String s) {
-        Stack<Integer> count = new Stack<>();
-        Stack<String> result = new Stack<>();
-        int i = 0;
-        result.push("");
-        while (i < s.length()) {
-            char ch = s.charAt(i);
-            if (ch >= '0' && ch <= '9') {
-                int start = i;
-                while (s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9') i++;
-                count.push(Integer.parseInt(s.substring(start, i + 1)));
-            } else if (ch == '[') {
-                result.push("");
-            } else if (ch == ']') {
-                String str = result.pop();
-                StringBuilder sb = new StringBuilder();
-                int times = count.pop();
-                for (int j = 0; j < times; j += 1) {
-                    sb.append(str);
-                }
-                result.push(result.pop() + sb.toString());
-            } else {
-                result.push(result.pop() + ch);
-            }
-            i += 1;
-        }
-        return result.pop();        
-    }
 
-    // private int pos = 0;
-    // public String decodeString(String s) {
-    //     StringBuilder sb = new StringBuilder();
-    //     String num = "";
-    //     for (int i = pos; i < s.length(); i++) {
-    //         if (s.charAt(i) != '[' && s.charAt(i) != ']' && !Character.isDigit(s.charAt(i))) {
-    //             sb.append(s.charAt(i));
-    //         } else if (Character.isDigit(s.charAt(i))) {
-    //             num += s.charAt(i);
-    //         } else if (s.charAt(i) == '[') {
-    //             pos = i + 1;
-    //             String next = decodeString(s);
-    //             for (int n = Integer.valueOf(num); n > 0; n--) sb.append(next);
-    //             num = "";
-    //             i = pos;
-    //         } else if (s.charAt(i) == ']') {
-    //             pos = i;
-    //             return sb.toString();
-    //         }
-    //     }
-    //     return sb.toString();
-    // }
+    public String decodeString(String s) {
+        
+        return null;    
+    }
     
 }
--- dfs/Solution99.java
+++ dfs/Solution99.java
@@ -1,109 +1,11 @@
-package dfs;
-
+package day3;
 import common.TreeNode;
+
 /**
  * Solution99
- * 
- * Two elements of a binary search tree (BST) are swapped by mistake.
-
-Recover the tree without changing its structure.
-
-Example 1:
-
-Input: [1,3,null,null,2]
-
-   1
-  /
- 3
-  \
-   2
-
-Output: [3,1,null,null,2]
-
-   3
-  /
- 1
-  \
-   2
-Example 2:
-
-Input: [3,1,4,null,null,2]
-
-  3
- / \
-1   4
-   /
-  2
-
-Output: [2,1,4,null,null,3]
-
-  2
- / \
-1   4
-   /
-  3
-Follow up:
-
-A solution using O(n) space is pretty straight forward.
-Could you devise a constant space solution?
-
  */
 public class Solution99 {
-   public void recoverTree(TreeNode root) {
-      if (root== null) {
-          return;
-      }
-      TreeNode largest = findLargest(root.left, root);
-      TreeNode smallest = findSmallest(root.right, root);       
-      if (largest.val > smallest.val) {  //two anomalies are found
-          swap(largest, smallest);
-      } else if (smallest.val < root.val) { //root is one of the swapped elements
-          swap(root, smallest);
-      } else if (largest.val > root.val) {  //root is one of the swapped elements
-          swap(largest, root); 
-      } else {  // swap has happened in one of the subtrees
-          recoverTree(root.left);
-          recoverTree(root.right);
-      }
-  }
-  
-
-      
-  TreeNode findLargest(TreeNode root, TreeNode max) {
-      if (root==null) return max;
-      if (root.val > max.val) {
-          max = root;
-      } 
-      TreeNode left = findLargest(root.left, max);
-      if (left.val > max.val) {
-          max = left;
-      }
-      TreeNode right = findLargest(root.right, max);
-      if (right.val > max.val) {
-          max = right;
-      }
-      return max;
-  }
-  
-  TreeNode findSmallest(TreeNode root, TreeNode min) {
-      if (root==null) return min;
-      if (root.val < min.val) {
-          min = root;
-      }
-      TreeNode left = findSmallest(root.left, min);
-      if (left.val < min.val) {
-          min = left;
-      }
-      TreeNode right = findSmallest(root.right, min);
-      if (right.val < min.val) {
-          min = right;
-      }
-      return min;
-  }
-  
-  void swap(TreeNode a, TreeNode b) {
-      int temp  = a.val;
-      a.val = b.val;
-      b.val = temp;
-  }
+    public void recoverTree(TreeNode root) {
+        
+    }    
 }
