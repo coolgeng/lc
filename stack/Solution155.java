ac78be632b3e604ed606c90ed77958d9435d06df
Cooper Geng <cooper.geng@fuhu.com>
2020-02-21 00:41:44 -0800

    add 155

---
+++ stack/Solution155.java
@@ -0,0 +1,30 @@
+package stack;
+
+/**
+ * Solution155
+ */
+public class Solution155 {
+    
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+
+}
