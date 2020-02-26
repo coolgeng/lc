62abbfbdc379f920a1f36fa17b3f3aa03e76bbea
yong geng <yong.geng@vudu.com>
2020-02-25 16:48:27 -0800

    add more linkedList cases

---
+++ linkedList/Solution138.java
@@ -0,0 +1,24 @@
+package linkedList;
+
+/**
+ * Solution138
+ */
+public class Solution138 {
+    
+    public Node copyRandomList(Node head) {
+        
+    }
+    
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
