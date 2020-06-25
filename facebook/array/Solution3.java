f2cf791b0cf8fc78ee36cdef9c20250438bf63c8
Cooper Geng <cooper.geng@fuhu.com>
2020-06-25 16:06:02 -0700

    add facebook questions

---
+++ facebook/array/Solution3.java
@@ -0,0 +1,22 @@
+package facebook.array;
+
+/**
+ * Solution3
+ */
+public class Solution3 {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0; 
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j-i);
+            }else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;  
+    }
+    
+}
--- facebook/array/Solution395.java
+++ facebook/array/Solution395.java
@@ -6,18 +6,8 @@ package facebook.array;
 public class Solution395 {
 
     public int longestSubstring(String s, int k) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0; 
-        while (i < n && j < n) {
-            if (!set.contains(s.charAt(j))) {
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j-i);
-            }else {
-                set.remove(s.charAt(i++));
-            }
-        }
-        return ans;   
+        
+          return 0;
     }
     
 }
